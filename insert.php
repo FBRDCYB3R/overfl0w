06fb71df145fbd6b2b1ca2b199670ab924d6185e
ABHax101 <72935354+ABHax101@users.noreply.github.com>
2021-10-10 15:56:14 -0400

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,1 +1,24 @@
+<?php
+    $msg = 'sample post lol haha';
+    $host = 'ec2-18-215-44-132.compute-1.amazonaws.com';
+    $username = 'rjsmgkwowkvldr';
+    $password = '9c4a10e89783466af42e3efa46540bf7680bc27ec270b65773e174690dbc60ac';
+    $database = 'dfi1ekl7pe7hfn';
 
+    try
+    {
+        $conn = new PDO("pgsql:host=$host;dbname=$database", $username, $password);
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $statement = $conn->prepare("INSERT INTO posts(post) VALUES(:msg)");
+        $statement->execute(array(
+            'msg' => $msg
+        ));
+    }
+    catch (PDOException $error)
+    {
+        $err = $error->getMessage();
+        die($err);
+    }
+?>
