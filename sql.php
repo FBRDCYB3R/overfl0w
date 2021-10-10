91d26366ae64193b73cc4f61e59aadcb69958fb8
ABHax101 <72935354+ABHax101@users.noreply.github.com>
2021-10-10 15:48:56 -0400

    Update sql.php

--- sql.php
+++ sql.php
@@ -2,14 +2,14 @@
     error_reporting(E_ALL);    
     ini_set('error_reporting', E_ALL);
 
-    $host = 'ec2-18-215-44-132.compute-1.amazonaws.com:5432';
-    $username = 'rjsmgkwowkvldr';
-    $password = '9c4a10e89783466af42e3efa46540bf7680bc27ec270b65773e174690dbc60ac';
-    $database = 'dfi1ekl7pe7hfn';
+    $host = '';
+    $username = '';
+    $password = '';
+    $database = '';
 
     try
     {
-        $conn = new PDO("pgsql:host=$host;dbname=$database", $username, $password);
+        $conn = new PDO("pgsql:host=ec2-18-215-44-132.compute-1.amazonaws.com:5432;dbname=dfi1ekl7pe7hfn", 'rjsmgkwowkvldr', '9c4a10e89783466af42e3efa46540bf7680bc27ec270b65773e174690dbc60ac');
 
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
