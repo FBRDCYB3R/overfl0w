f79554b0a2227af7494c1ea9b98ddd8c5b95d2d9
ABHax101 <72935354+ABHax101@users.noreply.github.com>
2021-10-10 16:27:00 -0400

    Create posts.php

---
+++ posts.php
@@ -0,0 +1,19 @@
+<html>
+    <style>
+        
+    </style>
+    <body>
+        <center>
+            <form method='POST'>
+                <input type='text' name='title' id='title' placeholder='Title your post' required minlength='1' maxlength='50' autocomplete='off'/><br><br>
+                <input type='text' name='description' id='description' placeholder='Description' required minlength='1' maxlength='420' autocomplete='off'/><br>
+                <textarea id='post' name='post' placeholder='What do you want to talk about?' required minlength='1' maxlength='42069' autocomplete='off'></textarea>
+            </form>
+        </center>
+    </body>
+</html>
+
+
+
+<?php
+?>
