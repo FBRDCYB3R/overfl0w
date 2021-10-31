1b775222155e8c2ed1209fede0d858bf6ca0ce37
ABHax101 <72935354+ABHax101@users.noreply.github.com>
2021-10-30 22:06:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //NavBar
-function hideIconBar(){
+function hideIconBar() {
     var iconBar = document.getElementById("iconBar");
     var navigation = document.getElementById("navigation");
     iconBar.setAttribute("style", "display:none;");
     navigation.classList.remove("hide");
 }
 
-function showIconBar(){
+function showIconBar() {
     var iconBar = document.getElementById("iconBar");
     var navigation = document.getElementById("navigation");
     iconBar.setAttribute("style", "display:block;");
@@ -14,13 +14,24 @@ function showIconBar(){
 }
 
 //Comment
-function showComment(){
+function showComment() {
     var commentArea = document.getElementById("comment-area");
     commentArea.classList.remove("hide");
 }
 
 //Reply
-function showReply(){
+function showReply() {
     var replyArea = document.getElementById("reply-area");
     replyArea.classList.remove("hide");
+}
+
+//Min/Max
+function max() {
+    document.getElementById("postmin").style.visibility = "none";
+    document.getElementById("post").style.visibility = "";
+}
+
+function min() {
+    document.getElementById("post").style.visibility = "none";
+    document.getElementById("postmin").style.visibility = "";
 }
