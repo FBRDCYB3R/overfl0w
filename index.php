dc87e7427e97a6e04451d55964a7f96caca0f3c0
FBRDCYB3R <85688976+FBRDCYB3R@users.noreply.github.com>
2021-11-26 21:16:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,13 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital@1&display=swap" rel="stylesheet">
     <script src="https://cdn.gravitec.net/storage/1316e80973603a0e8f3bc5b8dd821ca2/client.js" async></script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MRP4SS7');</script>
+    <!-- End Google Tag Manager -->
     <script type='text/javascript'>
         function min() {
             var min = document.getElementsByClassName("postmin");
